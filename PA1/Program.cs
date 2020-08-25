5085af18f2476f2d3746d1b3c7cb9f0ab89f5f01
ngdiroberto <60272425+ngdiroberto@users.noreply.github.com>
2020-08-25 15:03:13 -0500

    Add files via upload

---
+++ PA1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+
+            while(true){
+                PrintMenuOptions(); //Prints menu options to console
+                ValidateMenuChoice(ref input); //facilitates menu option selection
+                if(input = "4"){
+                    return;
+                }
+            }
+        }
+
+        public static void PrintMenuOptions(){
+            
+        }
+    }
+}
